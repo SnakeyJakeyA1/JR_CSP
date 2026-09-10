05fccfc02a8c04ae74eeaad163eefae149d77c88
SnakeyJakey <Jacob.rodriguez@ucas-edu.net>
2026-09-10 13:34:57 -0600

    variables practice

---
+++ variables_practice.py
@@ -0,0 +1,14 @@
+# JR variables practice
+
+name = ("Jake,")
+number = ("5,")
+hnumber = ("28,")
+breakfast = ("Oreo,")
+color = ("Deep blue,")
+school = ("UCAS,")
+year = ("2026,")
+eye = ("Brown,")
+age = ("16,")
+subject = ("CS principles.")
+
+print(name, number, hnumber, breakfast, color, school, year, eye, age, subject)
