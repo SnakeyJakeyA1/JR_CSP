b3040a3c02661b1770e12b9501d2429730cddfb0
SnakeyJakey <Jacob.rodriguez@ucas-edu.net>
2026-09-15 13:29:32 -0600

    fixing inputs

---
+++ physical_logical_memory.py
@@ -0,0 +1,32 @@
+# JR, Physical and Logical Memory
+
+pi = 3.141592
+
+everything = 42
+
+print(pi)
+print(f"The variable pi is {pi} and it is saved at {id(pi)}")
+print(f"The variable everything is {everything} and it is saved at {id(everything)}")
+
+name = "Jake"
+age = 16
+
+score1 = 10
+score2 = 25
+
+height = 5.7
+city = "Highland"
+zip_code = 84003
+
+price1 = 10
+price2 = 10
+
+print(f"The variable name is {name} and it is saved at {id(name)}")
+print(f"The variable age is {age} and it is saved at {id(age)}")
+print(f"The variable score1 is {score1} and it is saved at {id(score1)}")
+print(f"The variable score2 is {score2} and it is saved at {id(score2)}")
+print(f"The variable height is {height} and it is saved at {id(height)}")
+print(f"The variable city is {city} and it is saved at {id(city)}")
+print(f"The variable zip_code is {zip_code} and it is saved at {id(zip_code)}")
+print(f"The variable price1 is {price1} and it is saved at {id(price1)}")
+print(f"The variable price2 is {price2} and it is saved at {id(price2)}")
