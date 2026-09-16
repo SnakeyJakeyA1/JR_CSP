e95196c89c6f25b6371827ebb56e6476099775ce
SnakeyJakey <Jacob.rodriguez@ucas-edu.net>
2026-09-16 13:53:07 -0600

    Integers, Floats, and Expressions

---
+++ expressions.py
@@ -0,0 +1,36 @@
+# JR, Integers, Floats and Expressions Notes
+
+# Integers
+people = 18
+
+shoes = people * 2
+
+hand = people * 2
+
+hair = people * 100000
+
+# Floats
+pi = 3.141592
+
+water = 60.6
+
+temp = 68.5
+
+money = 1.99
+
+# Expressions
+print(water*money)
+print(f"Your {water} gallons of water costs ${round(water*money,2)}")
+
+# Modulo
+print(f"This is the result of 18%3 {18%3}")
+print(f"This is the result of 18%4 {18%4}")
+print(f"This is the result of 18%5 {18%5}")
+print(f"This is the result of 18%6 {18%6}")
+
+# Modulo and Integer division
+print(f"18/4 = {18/4} which is {18//4} with a remainder of {18%4}")
+print(f"18/4 = {18/5} which is {18//5} with a remainder of {18%5}")
+print(f"18/4 = {18/6} which is {18//6} with a remainder of {18%6}")
+
+print(int(people*2*(5-1)/3))
